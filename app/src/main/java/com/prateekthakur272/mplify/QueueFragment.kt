d37f9557ac851c19cfb03b9946faefe88c371cc4
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2023-02-21 01:14:50 +0530

    enables view binding in fragments

--- app/src/main/java/com/prateekthakur272/mplify/QueueFragment.kt
+++ app/src/main/java/com/prateekthakur272/mplify/QueueFragment.kt
@@ -5,17 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.prateekthakur272.mplify.databinding.FragmentQueueBinding
 
 class QueueFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    private lateinit var binding: FragmentQueueBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_queue, container, false)
+        binding = FragmentQueueBinding.inflate(inflater,container,false)
+
+        return binding.root
     }
 
 }
