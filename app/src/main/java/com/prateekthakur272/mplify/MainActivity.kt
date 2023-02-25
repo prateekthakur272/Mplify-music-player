19799c582e418f9e9efb46a7353b9c3564799d40
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2023-02-25 20:17:38 +0530

    adds navigation in by jetpack navigation library

--- app/src/main/java/com/prateekthakur272/mplify/MainActivity.kt
+++ app/src/main/java/com/prateekthakur272/mplify/MainActivity.kt
@@ -2,7 +2,8 @@ package com.prateekthakur272.mplify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.prateekthakur272.mplify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,41 +12,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        if(savedInstanceState != null) return
 
-        supportActionBar?.title = getString(R.string.player)
-        supportFragmentManager.commit {
-            replace(R.id.fragment_container,PlayerFragment())
-        }
+//        supportActionBar?.title = getString(R.string.player)
+//        supportFragmentManager.commit {
+//            replace(R.id.fragment_container,PlayerFragment())
+//        }
+//
+//        binding.mainNavBar.selectedItemId = R.id.action_player
+//
+//        binding.mainNavBar.setOnItemSelectedListener { item ->
+//            when(item.itemId){
+//                R.id.action_player -> {
+//                    supportFragmentManager.commit {
+//                        replace(R.id.fragment_container,PlayerFragment())
+//                    }
+//                    supportActionBar?.title = getString(R.string.player)
+//                    return@setOnItemSelectedListener true
+//                }
+//                R.id.action_music_queue -> {
+//                    supportFragmentManager.commit {
+//                        replace(R.id.fragment_container,QueueFragment())
+//                    }
+//                    supportActionBar?.title = getString(R.string.queue)
+//                    return@setOnItemSelectedListener true
+//                }
+//                R.id.action_music_list -> {
+//                    supportFragmentManager.commit {
+//                        replace(R.id.fragment_container,AllSongsFragment())
+//                    }
+//                    supportActionBar?.title = getString(R.string.all_songs)
+//                    return@setOnItemSelectedListener true
+//                }
+//                else -> {
+//                    return@setOnItemSelectedListener false
+//                }
+//            }
+//        }
 
-        binding.mainNavBar.selectedItemId = R.id.action_player
-
-        binding.mainNavBar.setOnItemSelectedListener { item ->
-            when(item.itemId){
-                R.id.action_player -> {
-                    supportFragmentManager.commit {
-                        replace(R.id.fragment_container,PlayerFragment())
-                    }
-                    supportActionBar?.title = getString(R.string.player)
-                    return@setOnItemSelectedListener true
-                }
-                R.id.action_music_queue -> {
-                    supportFragmentManager.commit {
-                        replace(R.id.fragment_container,QueueFragment())
-                    }
-                    supportActionBar?.title = getString(R.string.queue)
-                    return@setOnItemSelectedListener true
-                }
-                R.id.action_music_list -> {
-                    supportFragmentManager.commit {
-                        replace(R.id.fragment_container,AllSongsFragment())
-                    }
-                    supportActionBar?.title = getString(R.string.all_songs)
-                    return@setOnItemSelectedListener true
-                }
-                else -> {
-                    return@setOnItemSelectedListener false
-                }
-            }
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.mainNavBar.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/prateekthakur272/mplify/PlayerFragment.kt
+++ app/src/main/java/com/prateekthakur272/mplify/PlayerFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.prateekthakur272.mplify.databinding.FragmentPlayerBinding
+import com.prateekthakur272.mplify.viewmodel.PlayerViewModel
 
 class PlayerFragment : Fragment() {
     private lateinit var binding: FragmentPlayerBinding
+    private val viewModel by viewModels<PlayerViewModel>()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentPlayerBinding.inflate(inflater,container,false)
-        with(binding.buttonPausePlay){
+        viewModel.title.observe(viewLifecycleOwner){
+            binding.title.text = viewModel.title.value
+        }
+
+        with(binding.buttonPlayPause){
             setOnClickListener {
-                
+                viewModel.changeTitle("Play")
+            }
+        }
+        with(binding.buttonPrev){
+            setOnClickListener {
+                viewModel.changeTitle("prev")
+            }
+        }
+        with(binding.buttonNext){
+            setOnClickListener {
+                viewModel.changeTitle("next")
             }
         }
         return binding.root
